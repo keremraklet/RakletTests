c29434870f83bc5553f719fed654b877684a7d7c
keremraklet <91527036+keremersann@users.noreply.github.com>
2022-02-17 10:00:25 +0300

    TestHomePage.cs is refactored according to POM

--- RakletUI.Tests/TestHomePage.cs
+++ RakletUI.Tests/TestHomePage.cs
@@ -9,34 +9,15 @@ namespace RakletTests
 {
     public class TestHomePage
     {
-
-        private readonly string HomeUrl = "https://hello.raklet.net/";
-        private readonly string HomeTitle = "Membership Management Software for Associations, " +
-            "Clubs, Non-Profits, Chambers, Alumni";
-
-
-        //[Fact]
-        //public void Test1()
-        //{
-        //    using(IWebDriver driver = new ChromeDriver())
-        //    {
-        //        driver.Navigate().GoToUrl(HomeUrl);
-        //        IWebElement loginButton = driver.FindElement(By.XPath("//a[@href='https://login.raklet.net/?']"));
-        //        loginButton.Click();
-                
-        //    }
-        //}
-
         [Fact]
         public void LoadHomePage()
         {
             using(IWebDriver driver = new ChromeDriver())
             {
-                
-                driver.Navigate().GoToUrl(HomeUrl);
 
-                Assert.Equal(HomeUrl, driver.Url);
-                Assert.Equal(HomeTitle, driver.Title);
+                var homePage = new HomePage(driver);
+                homePage.NavigateTo();
+
             }
         }
 
@@ -45,12 +26,12 @@ namespace RakletTests
         {
             using(IWebDriver driver = new ChromeDriver())
             {
-                driver.Navigate().GoToUrl(HomeUrl);
+                var homePage = new HomePage(driver);
+                homePage.NavigateTo();
 
                 driver.Navigate().Refresh();
 
-                Assert.Equal(HomeUrl, driver.Url);
-                Assert.Equal(HomeTitle, driver.Title);
+                homePage.EnsurePageLoaded();
             }
         }
     }
